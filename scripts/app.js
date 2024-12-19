22684bb787502031d764eac8e18a9cf1a35a5034
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-12-19 15:02:54 +0500

    final changes

--- scripts/app.js
+++ scripts/app.js
@@ -136,31 +136,107 @@ let data = [
   },
 ];
 
+
+const lineLength = 100 / data.length;
+
 let questionEle = document.querySelector(".questionEle");
 let optionDiv = document.querySelector(".optionDiv");
 let nextBtn = document.querySelector(".next-btn");
+let showResult = document.querySelector(".showResult");
+
+let modalPage = document.querySelector(".modal-page");
+
+let body = document.getElementsByTagName("body")[0];
+
+let QuesHead = document.getElementById("Ques-Head");
+let line = document.getElementById("line");
+
+let min = document.getElementById("min");
+min.innerText = "Min: 0%";
+
+let barScore = document.getElementById("score");
+barScore.innerText = "Score: 0%";
+
+let max = document.getElementById("max");
+max.innerText = `Max: ${100}%`;
+
+let validity = document.getElementById("alert");
+let popUp = document.getElementById("pop-up");
+popUp.style.visibility = "hidden";
+
+let minLine = document.getElementById("minline");
+let scoreLine = document.getElementById("scoreline");
+let maxLine = document.getElementById("maxline");
+minLine.style.width = "0%";
+scoreLine.style.width = "0%";
+maxLine.style.width = `100%`;
+
+let restart = document.getElementById("restart");
 
 let count = 0;
 let score = 0;
+let num = 1;
+let time = document.getElementById("time");
 
 function showQuestion() {
   const currQuestion = data[count];
-  questionEle.innerHTML = `${count + 1}. ${currQuestion.question}`;
+  questionEle.innerHTML = ` ${currQuestion.question}`;
+  line.style.width = `${lineLength * num}%`;
+
+  QuesHead.innerText = `Question ${num > 9 ? num : "0" + num}/${data.length}`;
+  num++;
 
   optionDiv.innerHTML = "";
   for (let i = 0; i < currQuestion.options.length; i++) {
     const option = document.createElement("button");
     option.classList.add("question-btn");
+    option.classList.add("buttons");
     option.textContent = currQuestion.options[i];
 
     option.addEventListener("click", (e) => {
       if (e.target.textContent === currQuestion.answer) {
-        e.target.style.backgroundColor = "green";
+        e.target.style.backgroundColor = "transparent";
+        e.target.style.border = "1px solid rgb(38, 171, 171)";
+        restart.style.display = "none";
         e.target.style.color = "white";
+        validity.innerText = "Correct!";
+        popUp.style.visibility = "visible";
         score++;
+        min.innerText = `Min:${lineLength * score}%`;
+        minLine.style.width = `${lineLength * score}%`;
+        barScore.innerText = `Score: ${((score / (num - 1)) * 100).toFixed(
+          1
+        )}%`;
+        scoreLine.style.width = ` ${((score / (num - 1)) * 100).toFixed(1)}%`;
+        max.innerText = `Max: ${(
+          ((score + (data.length - (num - 1))) / data.length) *
+          100
+        ).toFixed(1)}%`;
+        maxLine.style.width = `${(
+          ((score + (data.length - (num - 1))) / data.length) *
+          100
+        ).toFixed(1)}%`;
       } else {
-        e.target.style.backgroundColor = "red";
+        e.target.style.backgroundColor = "transparent";
+        e.target.style.border = "1px solid red";
         e.target.style.color = "white";
+        validity.innerText = "Sorry!";
+        restart.style.display = "none";
+
+        popUp.style.visibility = "visible";
+        max.innerText = `Max: ${(
+          ((score + (data.length - (num - 1))) / data.length) *
+          100
+        ).toFixed(1)}%`;
+        barScore.innerText = `Score: ${((score / (num - 1)) * 100).toFixed(
+          1
+        )}%`;
+
+        scoreLine.style.width = ` ${((score / (num - 1)) * 100).toFixed(1)}%`;
+        maxLine.style.width = `${(
+          ((score + (data.length - (num - 1))) / data.length) *
+          100
+        ).toFixed(1)}%`;
       }
 
       const buttons = optionDiv.querySelectorAll("button");
@@ -173,20 +249,76 @@ function showQuestion() {
   }
 }
 
-function nextQuestion() {
-  count++;
-  if (count < data.length) {
-    showQuestion();
-  } else {
-    let result = document.querySelector(".result");
-    result.innerHTML = `You answered ${score} out of 20 questions correctly!`;
-  }
-}
-
 function reset() {
   location.reload();
 }
 
+function nextQuestion(e) {
+  restart.style.display = "none";
+  time.textContent = "";
+  timer();
+  count++;
+  console.log(num, score);
+  if (num == data.length) {
+    e.target.innerText = "Show Result";
+    console.log(num, score);
+    restart.style.display = "none";
+  }
+  if (count < data.length) {
+    popUp.style.visibility = "hidden";
+    console.log(num, score);
+    restart.style.display = "none";
+    showQuestion();
+  } else {
+    showResult.innerText = "";
+    body.setAttribute("class", "flex");
+    showResult.innerHTML = `<div class="result-page">
+    <h1>Result</h1>
+    <img src="./images/award.webp" width="200px" height="auto" />
+    <h2>${
+      score > data.length / 2 ? "Congratulations!" : "Better Luck Next Time"
+    }</h2>
+    <p>You answered ${score} out of ${
+      data.length
+    } questions correctly!<br/> Your percentage is ${(
+      (score / data.length) *
+      100
+    ).toFixed(0)}%</p>
+    <button onclick="reset()" class="reset-btn" id="restart">Restart Quiz</button>
+    </div>`;
+
+    e.target.style.display = "none";
+    restart.style.display = "inline-block";
+    console.log(num, score);
+  }
+}
+// Timer Functions
+let countdown;
+let timeLeft = 30;
+function timer() {
+  timeLeft = 30;
+  time.innerHTML = timeLeft;
+
+  clearInterval(countdown);
+
+  countdown = setInterval(() => {
+    timeLeft--;
+    time.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      time.textContent = "Time's up! Move to next question";
+      popUp.style.visibility = "visible";
+      const buttons = optionDiv.querySelectorAll("button");
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+      
+    }
+  }, 1000);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   showQuestion();
+  timer();
 });
